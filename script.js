4d2314e4c0eb6c74de366ff2408a8a4d23362f9d
Johan Rende <johan@rende.se>
2018-12-01 14:49:37 +0100

    Removed more scripts

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 ::MyCoolScript [script]
 
-window.addEventListener('load', function() {
-  console.log("document is ready");
+function createTextSequence(elmId) {
+  var elm = document.querySelector(elmId);
+  var index = 0;
+  console.log(elm);
+}
+/*
+elm.childNodes.forEach(function(node) {
+  node.style.display = 'none';
 });
+elm.childNodes[0].style.display = '';
+document.querySelector('#next').addEventListener('click', function(event) {
+  console.log('tjo');
+});
+*/
