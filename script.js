a9736b74d7fbf3de0079aaed50238ce66b8471f4
Johan Rende <johan@rende.se>
2018-12-01 15:08:59 +0100

    Asd

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-<%
-function createTextSequence(elmId) {
+%
+window.createTextSequence = function(elmId) {
   var elm = document.querySelector(elmId);
   var index = 0;
   console.log('elmId: ', elmId);
   console.log('elm: ', elm);
+  elm.children.forEach(function(node) {
+    node.style.display = 'none';
+  });
+  elm.children[0].style.display = '';
+  document.querySelector('#next').addEventListener('click', function(event) {
+    console.log('tjo');
+  });
 }
-%>
-/*
+  %>
+  /*
 elm.childNodes.forEach(function(node) {
   node.style.display = 'none';
 });
